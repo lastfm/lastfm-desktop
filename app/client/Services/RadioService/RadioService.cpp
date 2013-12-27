192b979c57aa0b9a2df4f11ad4851ce4fbf5d38b
Spiek <spiek@ymail.com>
2013-12-27 13:27:11 +0100

    Fix missing Cast

--- app/client/Services/RadioService/RadioService.cpp
+++ app/client/Services/RadioService/RadioService.cpp
@@ -307,7 +307,7 @@ RadioService::mute()
 void
 RadioService::onPhononStateChanged( Phonon::State newstate, Phonon::State oldstate )
 {
-    qDebug() << oldstate << " -> " << newstate;
+    qDebug() << (int)oldstate << " -> " << (int)newstate;
     if (m_mediaObject == 0) {
         qDebug() << "m_mediaObject is null!";
         return;
